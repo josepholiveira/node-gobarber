2261eb65eaddf88e02810e4b1086863c99ebafef
Joseph Oliveira <josephvalenteoliveira@gmail.com>
2019-07-11 11:48:34 -0300

    Implementando envio de e-mail a partir do cancelamento de agendamento

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '26576847c1e711',
+    pass: '90558722c59c78',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com',
+  },
+};
